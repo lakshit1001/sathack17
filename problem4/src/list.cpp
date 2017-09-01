ad50af498bd32be9419efb412bbd871870518d4b
purvanshi <purumehta69@gmail.com>
2017-09-02 04:41:12 +0530

    ls and pwd added

---
+++ problem4/src/list.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <string>
+#include <unistd.h>
+#include <cstdlib>
+#include <vector>
+#include <sstream>
+#include <sys/wait.h>
+#include <string.h>
+#include <stdio.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <iostream>
+
+
+void printdir (char *dir);
+int k_execute(int argc,char **cmd);
+
+int k_execute(int argc,char **cmd){
+  //char *args[2]={cmd,NULL};
+  //if(cmd==NULL)
+  char *cwd;
+  std::cout<<cmd[0];
+    if (argc>1){
+      cwd=cmd[1];
+    }else{    
+      cwd=".";
+      // if (getcwd(cwd, sizeof(cwd)) != NULL)
+          // fprintf(stdout, "Current working dir: %s\n", cwd);   
+    }
+  printdir(cwd);
+  return 0;
+}
+
+void printdir (char *dir) {
+    struct dirent *entry;
+    DIR *dp = opendir (dir);
+
+    if (dp == NULL) {
+        fprintf (stderr, "Cannot open dir:%s\n", dir);
+        return;
+    }
+
+    chdir (dir);
+    while ((entry = readdir(dp)) != NULL)
+        printf ("%s\n",entry -> d_name);
+    closedir (dp);
+}
+
+int main(int argc, char *argv[])
+{
+  /* code */
+  std::cout<<argc<<std::endl;
+  k_execute(argc,argv);
+  return 0;
+}
---
+++ problem4/src/pwd.cpp
@@ -0,0 +1,14 @@
+char* find_path(){
+char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL)
+       fprintf(stdout, "Current working dir: %s\n", cwd);
+   return cwd;
+}
+
+int main(int argc, char const *argv[])
+{
+	char cwd[1024];
+	cwd=find_path();
+	cout<<cwd
+	return 0;
+}
